866f5d03905f8c4d6166f56d62489be7deceb856
osama-hawas <ohawas02@gmail.com>
2023-04-29 03:09:52 +0300

    finish ui

--- lib/features/search_screen/views/widgets/search_text_field.dart
+++ lib/features/search_screen/views/widgets/search_text_field.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:go_router/go_router.dart';
 
 class SearchTextField extends StatefulWidget {
